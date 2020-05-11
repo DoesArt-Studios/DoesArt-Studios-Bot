e2a7312aaf62faa5e52ba57679cd70ba8fde9821
BrodyPlaysYT <63876947+BrodyPlaysYT@users.noreply.github.com>
2020-05-11 19:38:55 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,12 +19,6 @@ async def change_status():
     await client.change_presence(activity=discord.Game(next(status)))
 
 @client.command()
-async def art(ctx):
-    embed=discord.Embed(color=0xff8000)
-    embed.set_image(url="https://cdn.discordapp.com/attachments/708861766389596225/709267643030568981/art.png?size=1024")
-    await ctx.send(embed=embed)
-
-@client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
@@ -83,63 +77,69 @@ async def unban(ctx, *, member):
             return
 
 @client.command()
+async def art(ctx):
+    embed=discord.Embed(color=0xff8000)
+    embed.set_image(url="https://cdn.discordapp.com/attachments/708861766389596225/709267643030568981/art.png?size=1024")
+    await ctx.send(embed=embed)
+
+@client.command()
 async def art1(ctx):
     embed=discord.Embed(color=0xff8000)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/708861766389596225/709267643030568981/art.png?size=1024")
+    embed.set_image(url="https://cdn.discordapp.com/attachments/708861766389596225/709267643030568981/art.png?size=1024")
     await ctx.send(embed=embed)
 
 @client.command()
 async def art2(ctx):
     embed=discord.Embed(color=0xff8000)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/353076698386006016/708473875322896414/image0.png?size=1024")
+    embed.set_image(url="https://cdn.discordapp.com/attachments/353076698386006016/708473875322896414/image0.png?size=1024")
     await ctx.send(embed=embed)
 
 @client.command()
 async def art3(ctx):
     embed=discord.Embed(color=0xff8000)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/353076698386006016/709086588398731365/image0.png?size=1024")
+    embed.set_image(url="https://cdn.discordapp.com/attachments/353076698386006016/709086588398731365/image0.png?size=1024")
     await ctx.send(embed=embed)
 
 @client.command()
 async def art4(ctx):
     embed=discord.Embed(color=0xff8000)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/353076698386006016/707710404540760074/image0.png?size=1024")
+    embed.set_image(url="https://cdn.discordapp.com/attachments/353076698386006016/707710404540760074/image0.png?size=1024")
     await ctx.send(embed=embed)
 
 @client.command()
 async def art5(ctx):
     embed=discord.Embed(color=0xff8000)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/474808181579841546/708782836852850719/image0.png?size=1024")
+    embed.set_image(url="https://cdn.discordapp.com/attachments/474808181579841546/708782836852850719/image0.png?size=1024")
     await ctx.send(embed=embed)
 
 @client.command()
 async def art6(ctx):
     embed=discord.Embed(color=0xff8000)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/474808181579841546/708673865198600252/image0.png?size=1024")
+    embed.set_image(url="https://cdn.discordapp.com/attachments/474808181579841546/708673865198600252/image0.png?size=1024")
     await ctx.send(embed=embed)
 
 @client.command()
 async def art7(ctx):
     embed=discord.Embed(color=0xff8000)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/474808181579841546/708569324667011112/image0.png?size=1024")
+    embed.set_image(url="https://cdn.discordapp.com/attachments/474808181579841546/708569324667011112/image0.png?size=1024")
     await ctx.send(embed=embed)
 
 
 @client.command()
 async def art8(ctx):
     embed=discord.Embed(color=0xff8000)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/474808181579841546/708565275213168680/image0.jpg?size=1024")
+    embed.set_image(url="https://cdn.discordapp.com/attachments/474808181579841546/708565275213168680/image0.jpg?size=1024")
     await ctx.send(embed=embed)
 
 @client.command()
 async def art9(ctx):
     embed=discord.Embed(color=0xff8000)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/474808181579841546/709208908035784845/image0.jpg?size=1024")
+    embed.set_image(url="https://cdn.discordapp.com/attachments/474808181579841546/709208908035784845/image0.jpg?size=1024")
     await ctx.send(embed=embed)
 
 @client.command()
-async def website(ctx):
-    await ctx.send('https://fanart.tv')
+async def discord(ctx):
+    await ctx.send('https://discord.gg/9teJTGf')
 
 @client.command()
 @commands.is_owner()
