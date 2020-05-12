0559cf7e44035e7733be42b09e9401cce291c53a
BrodyPlaysYT <63876947+BrodyPlaysYT@users.noreply.github.com>
2020-05-12 16:02:47 +1000

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config :
+    role_name = "ROLE NAME YOU WANT TO BE ASSIGNED"
+    message_id = "MESSAGE ID FOR REACTION VERIFIER"
+    prefix = "."
+    bot_token = "YOUR BOT TOKEN GOES HERE"
+    channel_id = "CHANNEL ID FOR REACTION VERIFIER"
